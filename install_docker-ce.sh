bc82c644c33a79df0437096c77565f0efaf4af85
Otnamrehus <97101355+otnamrehus@users.noreply.github.com>
2024-09-14 19:35:14 +0700

    Update install_docker-ce.sh

--- install_docker-ce.sh
+++ install_docker-ce.sh
@@ -7,7 +7,7 @@ sudo apt update && \
 sudo apt install docker-ce docker-ce-cli containerd.io docker-compose && \
 sudo groupadd docker  && \
 sudo usermod -aG docker $USER && \
-sudo systemctl start docker && \
+sudo systemctl restart docker && \
 sudo systemctl enable docker && \
 docker volume create portainer_data  && \
 docker run -d -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
